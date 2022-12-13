0bc6ff0f4a9d2a471fdff474f8112eb753850d83
Ryan Munz <ryan.munzy@gmail.com>
2022-12-13 17:59:41 -0500

    commit tree 02c06fe2613154cc0471aba32eb18b8ba96eec68

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    return customerName.toUpperCase();
+}
+    
+function setBestCustomer () {
+    var bestCustomer = 'not bob';
+}
